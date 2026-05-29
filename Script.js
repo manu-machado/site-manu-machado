4f9e0cda2528f0fb96adbf2479a8c8e84b77db64
manu-machado <manuella.machado14@escola.pr.gov.br>
2026-05-29 09:32:07 -0300

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,7 @@
+console.log("Site Agro Forte Sustentável carregado com sucesso!");
+
+const botao = document.querySelector(".btn");
+
+botao.addEventListener("click", () => {
+  alert("Bem-vindo ao Agro Forte Sustentável!");
+});
